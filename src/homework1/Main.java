6895b0bc39166313e6eee310a403e4397d0baf72
Aleksandr Kozliuk <aleksandr.kozliuk@gmail.com>
2021-09-03 12:09:49 +0300

    Master-6: add solution for task 5

--- src/homework1/Main.java
+++ src/homework1/Main.java
@@ -11,6 +11,14 @@ public class Main {
         /*----------ADDITIONAL TASK---------*/
         Address address = new Address();
 
+        address.setIndex(01011);
+        address.setCountry(" ,"+"Ukraine");
+        address.setCity(" ,"+"Kyiv");
+        address.setStreet(" ,"+"Khreschatyk");
+        address.setApartment(" ,"+"101");
+
+        System.out.println(address.toString());
+
         /*----------TASK 2---------*/
         System.out.println("!!! Solution for TASK 2 !!!");
         Rectangle rectangle = new Rectangle();
@@ -32,13 +40,13 @@ public class Main {
         // create massive computers with type of Computer Class
         Computer[] computers = new Computer[5];
 
-        // initialise massive with Computer Exemplars
+        // initialise massive with  Exemplars of Computer Class
         for (int i = 0; i < computers.length; i++) {
             computers[i] = new Computer();
         }
         // set values into variable computerName to massive
         for (int i = 0; i < computers.length; i++) {
-            computers[i].computerName="Name of computer " +i;
+            computers[i].computerName = "Name of computer " + i;
         }
         // show result into console
         for (Computer comp : computers) {
--- src/homework1/add_task/Address.java
+++ src/homework1/add_task/Address.java
@@ -1,4 +1,66 @@
 package homework1.add_task;
 
 public class Address {
+    int index;
+    String country, city, street, house, apartment;
+
+    @Override
+    public String toString() {
+        return "Address{" +
+                "index=" + index +
+                ", country='" + country + '\'' +
+                ", city='" + city + '\'' +
+                ", street='" + street + '\'' +
+                ", house='" + house + '\'' +
+                ", apartment='" + apartment + '\'' +
+                '}';
+    }
+
+    public int getIndex() {
+        return index;
+    }
+
+    public void setIndex(int index) {
+        this.index = index;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getHouse() {
+        return house;
+    }
+
+    public void setHouse(String house) {
+        this.house = house;
+    }
+
+    public String getApartment() {
+        return apartment;
+    }
+
+    public void setApartment(String apartment) {
+        this.apartment = apartment;
+    }
 }
